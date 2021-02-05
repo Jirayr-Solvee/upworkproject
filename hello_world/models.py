4b844b518b4d55379018e9eb0e2edcc3a5f28f27
jirayr-solvee <jirayr.melikyan@yahoo.com>
2021-02-05 17:38:03 +0400

    Add view url and html

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class SecondTable(models.Model):
+    some_text = models.TextField()
+
+    def __str__(self):
+        return self.some_text
+
+class FirstTable(models.Model):
+    some_text = models.TextField()
+    second_table = models.ForeignKey(SecondTable, related_name='second_table', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.some_text
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from . import models
+
+def hello_world(request):
+    table_2 = models.SecondTable.objects.get(id=1)
+    table_1 = table_2.second_table.all()
+    template = loader.get_template('hello_world/hello_world.html')
+
+    context = {
+        'table_1': table_1,
+        'table_2': table_2
+    }
+    return HttpResponse(template.render(context, request))
